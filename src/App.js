f70f2e9a69500178477faedbd9f1d407190e16a7
Sam Chamani <samchamani@Sams-MBP-2.fritz.box>
2022-05-02 23:39:42 +0200

    websocket button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import "./App.css";
+let ws = new WebSocket("ws://localhost:8025/websockets/game");
+
 function App() {
-  return <></>;
+  //   console.log(ws);
+  ws.onmessage = (e) => {
+    let message = e.data;
+    // console.log(message);
+    let parsedMessage = JSON.parse(message);
+    console.log("Message from server: \n" + message);
+  };
+
+  return (
+    <div className="login" onClick={handleClick}>
+      {"Login"}
+    </div>
+  );
 }
+
+const handleClick = () => {
+  ws.send(
+    JSON.stringify({
+      type: 0,
+      username: "Gruppe AI",
+    })
+  );
+};
 
 export default App;
